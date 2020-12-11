0058161014ff39c9dd2ba39be212faa73ed0eb65
Mauricio Lizcano <mauriciolizcano25531@gmail.com>
2020-12-10 20:17:30 -0400

    estamos listos

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,64 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
 //create your first component
-export function Home() {
+const Home = () => {
+	const [task, setTask] = useState("");
+	const [list, setList] = useState([]);
+	const deleteItems = indexItem => {
+		setList(prevState =>
+			prevState.filter((todo, index) => index !== indexItem)
+		);
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid col-8">
+			<div className="text-center">
+				<h1>To do list</h1>
+			</div>
+			<div className="group">
+				<input
+					placeholder="indique su tarea"
+					value={task}
+					onKeyDown={e => {
+						if (e.keyCode == 13) {
+							let newList = [];
+							for (let i = 0; i < list.length; i++) {
+								newList.push(list[i]);
+							}
+							newList.push(task);
+							setList(newList);
+							setTask((e.target.value = ""));
+						}
+					}}
+					onChange={e => {
+						setTask(e.target.value);
+					}}
+					type="text"
+				/>
+				<ul className="list-group list-group-flush">
+					{list.map((cosas, index) => {
+						return (
+							<li key={index} className="list-group-item">
+								{cosas}
+								<button
+									className="btn btn-light"
+									onClick={e => {
+										deleteItems(index);
+									}}>
+									<i
+										className="fa fa-times"
+										aria-hidden="true"
+									/>
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<div className="footer text-muted">
+				{list.length > 0
+					? `${list.length} tareas por completar`
+					: "Agregar tareas"}
+			</div>
 		</div>
 	);
-}
+};
+export default Home;
